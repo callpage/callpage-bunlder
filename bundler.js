462b94ce41907e8e16070f81fe5baeaf85ec3a25
adrianforcallpage <adrian.przybyla@callpage.io>
2017-07-01 20:25:48 +0200

    Create bundler.js

---
+++ bundler.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const isConfig = fs.existsSync(process.cwd() + '/config.js');
+
+
+if (!isConfig) {
+    console.log('\x1b[36m%s\x1b[0m: ', 'File \'config.js\' doesn\'t exists in current directory', process.cwd());
+} else {
+    var callpageAPI = require(process.cwd() + '/config.js');
+}
+
+require('./validator.js')(callpageAPI)
+
+if (!fs.existsSync(process.cwd() + '/.eslintrc.js')) {
+    fs.writeFileSync('.eslintrc.js', require('./esLintConfiguration.js')(callpageAPI), 'utf-8');
+} else {
+    throw new Error('.eslintrc.js exists')
+}
+
+if(!fs.existsSync(process.cwd() + '/webpack.config.js')) {
+    fs.writeFileSync('webpack.config.js', require('./callpage.webpack.js')(callpageAPI), 'utf-8')
+} else {
+    throw new Error('webpack.config.js exists')
+}
