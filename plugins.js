b83ef25e225e368a961d99dd0b237a1bbc111860
adrianforcallpage <adrian.przybyla@callpage.io>
2017-07-08 22:52:40 +0200

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -1,19 +1,37 @@
 class Plugins {
     constructor(configWepback) {
         this.configWepback = configWepback[0];
-        this.ngAnnotate = this.configWepback.js.plugins.ngAnnotate ? `const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');` : undefined;
-        this.copyFile = this.configWepback.copy.files.length > 0 ? `const CopyWebpackPlugin = require('copy-webpack-plugin');`: undefined;
-        this.purifyAndExtract = this.configWepback.scss ? `const PurifyCSSPlugin = require('purifycss-webpack'); const ExtractTextPlugin = require("extract-text-webpack-plugin");`: undefined;
-        this.plugins = [this.ngAnnotate, this.copyFile, this.purifyAndExtract].filter(plugin => plugin).join(' ')
+        this.template = `
+            const webpack = require('webpack');
+            const path = require('path');
+            const glob = require('glob');
+            const CleanWebpackPlugin = require('clean-webpack-plugin');`
     }
     getPlugins () {
-        let template = `const webpack = require('webpack');
-                                const path = require('path');
-                                const glob = require('glob');
-                                const CleanWebpackPlugin = require('clean-webpack-plugin');
-                                ${this.plugins}
-                                `
-        return template;
+        this.getNgAnnotatePlugin()
+        this.getCopyPlugin()
+        this.getScssPlugin()
+        return this.template;         
+    }
+
+    getNgAnnotatePlugin() {
+        if(this.configWepback.js.plugins) {
+            if(this.configWepback.js.plugins.ngAnnotate) {
+                this.template += `const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');`
+            }
+        }
+    }
+    getCopyPlugin() {
+        if(this.configWepback.copy) {
+            if(this.configWepback.copy.files.length > 0) {
+                this.template += `const CopyWebpackPlugin = require('copy-webpack-plugin');`
+            }
+        }
+    }
+    getScssPlugin() {
+        if(this.configWepback.scss) {
+            this.template += `const PurifyCSSPlugin = require('purifycss-webpack'); const ExtractTextPlugin = require("extract-text-webpack-plugin");`
+        }
     }
 }
 
@@ -21,5 +39,3 @@ module.exports = function (webpackConfig) {
     const plugin = new Plugins(webpackConfig)
     return plugin.getPlugins()
 }
-
-
